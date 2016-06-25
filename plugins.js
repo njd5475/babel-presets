60e9cf671c4fb4dd882dae193fc7aa24401d2624
spalger <email@spalger.com>
2016-06-25 05:37:42 -0700

    I guess class properties still need to be first

--- plugins.js
+++ plugins.js
@@ -5,6 +5,9 @@ module.exports = function (opts) {
   var amd = opts.amd
 
   return [
+    // I guess this still needs to be first...
+    require('babel-plugin-transform-class-properties'),
+
     // es2015
     require('babel-plugin-check-es2015-constants'),
     require('babel-plugin-transform-es2015-arrow-functions'),
@@ -30,7 +33,6 @@ module.exports = function (opts) {
 
     // stage 1
     require('babel-plugin-transform-class-constructor-call'),
-    require('babel-plugin-transform-class-properties'),
     require('babel-plugin-transform-decorators'),
     require('babel-plugin-transform-export-extensions'),
 
