4bb05ff422774e8c9a6c9f96872a6af3173685cb
spalger <email@spalger.com>
2016-06-25 01:57:50 -0700

    well, actually add no-module preset, rather than rename amd

--- amd.js
+++ amd.js
@@ -1,2 +1,6 @@
 // modified version of es2015 and stage-1 presets which use generators rather than regenertor
-module.exports = require('./commonjs')
+module.exports = {
+  plugins: require('./plugins')({
+    amd: true,
+  }),
+}
